5022bdaee55791c687ee7d2ca9261398948d2d6d
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-20 14:27:57 -0500

    notes added to shuffle array

---
+++ leetcode/arrays/easy/shuffle_the_array.py
@@ -0,0 +1,12 @@
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+# INPUTS/OUTPUTS
+#   nums = [2, 5, 1, 3, 4, 7], n = 3 => [2, 3, 5, 4, 1, 7]
+#   x = [2, 5, 1], y = [3, 4, 7]
+#   we want to place each of the y elements right after each of the x elements
+#   somewhat like a zip problem
+
+def shuffle(nums, n):
+    pass
