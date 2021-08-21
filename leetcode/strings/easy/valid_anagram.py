d8fe6095ce79b99fdffaeb8b95bde43099d0d46e
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-21 10:27:38 -0500

    valid anagram done

---
+++ leetcode/strings/easy/valid_anagram.py
@@ -0,0 +1,31 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+"""
+    INPUTS/OUTPUTS
+        s = "anagram", t = "nagaram"
+"""
+
+
+def isAnagram(s, t):
+    char_freq = {}
+
+    for char in s:
+        if char not in char_freq:
+            char_freq[char] = 1
+        char_freq[char] += 1
+
+    for char2 in t:
+        if char2 not in char_freq:
+            return False
+        else:
+            char_freq[char2] -= 1
+
+    for value in char_freq.values():
+        if value != 0:
+            return False
+    return True
+
+
+isAnagram("anagram", "nagaram")
+
+# Time complexity - O(n + m + y)
