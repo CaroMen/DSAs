d022b613d8dbc3bc64347875f638c9c70a834566
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-20 22:17:24 -0500

    rotate list started

---
+++ leetcode/linkedlists/medium/rotate_list.py
@@ -0,0 +1,20 @@
+# Given the head of a linked list, rotate the list to the right by k places.
+
+"""
+    INPUT/OUTPUT
+        [1, 2, 3, 4, 5], k = 2 => [4, 5, 1, 2, 3]
+
+    NOTES
+
+"""
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def rotateRight(self, head, k):
+        pass
