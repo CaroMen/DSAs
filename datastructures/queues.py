9b7e860ac5b3322c8d7265b90c1343560dea56a4
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-21 23:18:55 -0500

    queues fixed

--- datastructures/queues.py
+++ datastructures/queues.py
@@ -1,24 +1,50 @@
 # Create a queue using classes
 
+"""
+    LIFO - Last In, First Out
+    adding is called pushing
+    deleting is called pooping
+
+    works very similar to stacking plates
+    note:
+        when we add, the new node will point to the top and the top points to the new node
+
+        when we remove, we need to get the back. the back will now point to the top will now point to None and the new top will be the removed top's .next
+"""
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
 class Queue:
     def __init__(self):
         # difference between passing it in the constructor vs just assigning it like that
-        self.queue = []
-
-    def get_front(self):
-        self.queue[-1]
-
-    def get_end(self):
-        self.queue[0]
-
-    def is_empty(self):
-        if len(self.queue) == 0:
-            return True
-        else:
-            return False
-
-    def dequeue(self):
-        return self.queue.pop(0)
+        self.front = None
+        self.back = None
+        self.length = 0
 
     def enqueue(self, val):
-        return self.queue.append(val)
+        node = Node(val)
+        if self.length == 0:
+            self.front = node
+            self.back = node
+        else:
+            # we have a front
+            self.back.next = node
+            self.back = node
+
+        self.length += 1
+        return self.length
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+
+        removed = self.front
+        self.front = removed.next
+        self.length -= 1
+
+        return removed.val
