8fe0003db876a33c34876850bbe22eb2f30f13d4
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-21 23:48:14 -0500

    bst started

---
+++ datastructures/binary_search_tree.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, current_node=self.root):
+        pass
