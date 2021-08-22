de902900395ba5fef34d0f32a4e644d975e91c94
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-21 20:15:38 -0500

    merge intervals done. need to add notes

---
+++ leetcode/arrays/medium/merge_intervals.py
@@ -0,0 +1,26 @@
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+"""
+    INPUTS/OUTPUTS
+        [[1,3],[2,6],[8,10],[15,18]]
+        =>
+        [[1,6], [8,10], [15,18]]
+
+
+"""
+
+
+def mergeIntervals(intervals):
+    intervals.sort(key=lambda i: i[0])
+
+    res = [intervals[0]]
+
+    for start, end in intervals[1:]:
+        last_end = res[-1][1]
+
+        if start <= last_end:
+            res[-1][1] = max(end, last_end)
+        else:
+            res.append([start, end])
+
+    return res
