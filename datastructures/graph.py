0571514cdc60e662c13ab10cf3e80ec76f09da19
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-22 17:31:23 -0500

    graph implementation started, need dfs and bfs

--- datastructures/graph.py
+++ datastructures/graph.py
@@ -5,8 +5,8 @@ class Graph:
         self.adjList = {}
 
     def add_vertex(self, vertex):
-        if not self.adjList[vertex]:
-            self.adjList[vertext] = []
+        if vertex not in self.adjList:
+            self.adjList[vertex] = []
 
     def add_edges(self, source_value, dest_value):
         self.add_vertex(source_value)
@@ -14,8 +14,9 @@ class Graph:
 
         self.adjList[source_value].append(dest_value)
         self.adjList[dest_value].append(source_value)
+        # print(self.adjList)
 
-    def build_graph(edges):
+    def build_graph(self, edges):
         for edge in edges:
             if len(edge) == 1:
                 self.add_vertex(edge[0])
@@ -23,3 +24,29 @@ class Graph:
                 self.add_edges(edge[0], edge[1])
 
         return self.adjList
+
+
+edgesa = [['a', 'b'],
+          ['c']]
+#   ['a', 'd'],
+#   ['d', 'g'],
+#   ['b', 'c'],
+#   ['b', 'e'],
+#   ['c', 'f'],
+#   ['c', 'g'],
+#   ['f', 'g']]
+
+graph = Graph()
+
+print(graph.build_graph(edgesa))
+"""
+{
+    'a': ['b', 'c', 'd'],
+    'b': ['a', 'c', 'e'],
+    'c': ['a', 'b', 'f', 'g'],
+    'd': ['a', 'g'],
+    'g': ['d', 'c', 'f'],
+    'e': ['b'],
+    'f': ['c', 'g']}
+
+"""
