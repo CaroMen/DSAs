735c82bc5944dcc9ee59f5142fd3d9f58a84a1bd
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-23 21:19:18 -0500

    word wrap not fully done

---
+++ leetcode/strings/easy/word_wrap.py
@@ -0,0 +1,40 @@
+# NOT LEETCODE
+# Given a string, write an algorithm to wrap the whole string so that not more than 8 characters are on one line. Also you have to make sure you don't break/split any word into two. Spaces are considered characters as well.
+
+"""
+    INPUTS/OUTPUTS:
+        "Hello, my name is Caroline and I am a junior engineer"
+        =>
+        "Hello,
+        my name
+        is
+        Caroline
+        and I am
+        a junior
+        engineer"
+
+    NOTES:
+        we could loop through the string or create a loop through 8
+            so: for i in range(8):
+            * this wouldn not loop through the entire string
+        we have to check that the words aren't being split
+            not sure how *
+
+"""
+
+
+def word_wrap(string, max_width):
+    res = []
+
+    for i in range(len(string)):
+        new_line = string[:max_width]
+        res.append(new_line)
+        string = string[max_width:]
+
+        if len(new_line) < max_width:
+            break
+
+    print('\n'.join(res))
+
+
+word_wrap("Hello, my name is Caroline and I am a junior engineer", 8)
