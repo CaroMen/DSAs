5ec601f449ad97beeef8dc504540e96998ff4e82
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-28 14:39:19 -0500

    missing number walkthrough

--- leetcode/arrays/easy/missing_number.py
+++ leetcode/arrays/easy/missing_number.py
@@ -20,6 +20,36 @@
         loop from 0 to nums_length
         if the idx not in nums:
             return idx
+
+        second approach:
+            loop through from range 0 to length of nums
+            have a length variable and a variable to count total sum of the values inside the nums list
+            inside the loop, do sum += nums[idx]
+            and also increment the length of the list by idx
+            return length - sum
+
+            [0, 2, 3, 4]
+                - length is 4
+                - missing num is 1
+                loop 1:
+                    idx - 0
+                    sum - 0
+                    length - 4
+                loop 2:
+                    idx - 1
+                    sum - 2
+                    length - 5 (idx + length)
+                loop 3:
+                    idx - 2
+                    sum - 5
+                    length - 7 (idx + length)
+                loop 4:
+                    idx - 3
+                    sum - 9
+                    length - 10 (idx + length)
+
+                return lenth (now 10) - sum (9) = 1, our missing number
+
 """
 
 
