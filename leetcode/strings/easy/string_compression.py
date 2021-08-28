6bb0014d8d22dc20f921186ec146a23396dddfa5
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-28 07:49:30 -0500

    string compression done

--- leetcode/strings/easy/string_compression.py
+++ leetcode/strings/easy/string_compression.py
@@ -9,6 +9,29 @@
     NOTES
         - we have to somehow keep count of how many times a letter appears
         - we should only keep count while the consecutive letters match
+        - we can use an index pointer to help us loop through the string
         - if the counter is just equal to 1, then we can just add the letter to our new result
         - else, we add the letter plus the string version of our counter
 """
+
+
+def stringCompression(string):
+    index = 0
+    res = ''
+
+    while index < len(string):
+        appeareance_counter = 0
+        string_length = len(string)
+
+        while ((index < string_length - 1) and string[index] == string[index + 1]):
+            appeareance_counter += 1
+            index += 1
+
+        if appeareance_counter == 1:
+            res += string[index]
+        else:
+            res += string[index] + str(appeareance_counter)
+
+        index += 1
+
+    return res
