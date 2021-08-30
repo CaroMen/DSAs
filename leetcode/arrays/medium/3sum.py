cd8b2d52d11e1a0c28da7079833d4e2a98a42f37
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-30 14:44:41 -0500

    3sum done

--- leetcode/arrays/medium/3sum.py
+++ leetcode/arrays/medium/3sum.py
@@ -13,3 +13,27 @@
     NOTES
         brute force solution would be nested loops to get 3 numbers, make sure each doesnt equal to each other, and append them to a list
 """
+
+
+def threeSum(nums):
+    res = []
+    nums.sort()
+
+    for i, num in enumerate(nums):
+        if i > 0 and num == nums[i - 1]:
+            continue
+
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            three_sum = num + nums[left] + nums[right]
+            if three_sum > 0:
+                right -= 1
+            elif three_sum < 0:
+                left += 1
+            else:
+                res.append([num, nums[left], nums[right]])
+                left += 1
+                while nums[left] == nums[left - 1] and left < right:
+                    left += 1
+
+    return res
