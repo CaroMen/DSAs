bc42507039ff76731427a3d5d9df12f64d33f8e8
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-31 20:23:20 -0500

    middle of linked list

--- leetcode/linkedlists/easy/middle_of_linkedlist.py
+++ leetcode/linkedlists/easy/middle_of_linkedlist.py
@@ -14,3 +14,20 @@
         - we might be able to loop as normal with one pointer moving two ahead and the other moving one at a time
         - then once the fast pointer reaches the end, our slow pointer should be the halfway point (?)
 """
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head):
+        fast = head
+        slow = head
+        while fast is not None and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
