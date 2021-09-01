94a67aea77a5c83160683417bb95ec3906d8be6f
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-31 19:08:11 -0500

    notes for middle of linked list started

---
+++ leetcode/linkedlists/easy/middle_of_linkedlist.py
@@ -0,0 +1,16 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
+"""
+    INPUTS/OUTPUTS
+        head = [1, 2, 3, 4, 5] => [3, 4, 5]
+
+        head = [1, 2, 3, 4, 5, 6] => [4, 5, 6]
+
+    NOTES
+        - if we have two middle points, we need to return the second middle
+        - we can use the fast and slow pointer pattern
+        - we might be able to loop as normal with one pointer moving two ahead and the other moving one at a time
+        - then once the fast pointer reaches the end, our slow pointer should be the halfway point (?)
+"""
