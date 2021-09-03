6d2ca16754df3bd97382bf8693c150d9600eb54a
Caro <55109108+CaroMen@users.noreply.github.com>
2021-09-02 19:56:05 -0500

    flipping an image problem started

---
+++ leetcode/arrays/easy/flipping_an_image.py
@@ -0,0 +1,34 @@
+# Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+# To flip an image horizontally means that each row of the image is reversed.
+
+# For example, flipping [1,1,0] horizontally results in [0,1,1].
+# To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+# For example, inverting [0,1,1] results in [1,0,0].
+
+
+"""
+    INPUTS/OUTPUTS
+        image = [
+            [1, 1, 0],
+            [1, 0, 1],
+            [0, 0, 0]
+        ]
+        =>
+        [
+            [1, 0, 0],
+            [0, 1, 0],
+            [1, 1, 1]
+        ]
+
+    NOTES
+        each 1 needs to be turned into a 0
+        each 0 needs to be turned into a 1
+        the values inside the inner lists have to be reversed
+
+"""
+
+
+def flipAndInvert(image):
+    pass
