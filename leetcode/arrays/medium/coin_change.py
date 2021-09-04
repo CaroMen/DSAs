e8bf2c6d7ebb313fd26d2ebfd253f03720c493d4
Caro <55109108+CaroMen@users.noreply.github.com>
2021-09-04 17:24:24 -0500

    coin change done. need to review

--- leetcode/arrays/medium/coin_change.py
+++ leetcode/arrays/medium/coin_change.py
@@ -3,3 +3,14 @@
 # Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
 # You may assume that you have an infinite number of each kind of coin.
+
+def coinChange(coins, amount):
+    dp = [amount + 1] * (amount + 1)
+    dp[0] = 0
+
+    for amt in range(1, amount + 1):
+        for coin in coins:
+            if amt - coin >= 0:
+                dp[amt] = min(dp[amt], 1 + dp[amt - coin])
+
+    return dp[amount] if dp[amount] != amount + 1 else -1
