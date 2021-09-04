41e2a380ce4af2976cbc95c80c9cbb2eb34db706
Caro <55109108+CaroMen@users.noreply.github.com>
2021-09-04 15:28:15 -0500

    optimized grid traveler

--- dynamicprogrammingnotes/grid_traveler.py
+++ dynamicprogrammingnotes/grid_traveler.py
@@ -12,8 +12,30 @@ def grid_traveler(m, n):
     return grid_traveler(m - 1, n) + grid_traveler(m, n - 1)
 
 
-print(grid_traveler(1, 1))  # 1
-print(grid_traveler(2, 3))  # 3
-print(grid_traveler(3, 2))  # 3
-print(grid_traveler(3, 3))  # 6
-print(grid_traveler(18, 18))  # 2333606220
+# print(grid_traveler(1, 1))  # 1
+# print(grid_traveler(2, 3))  # 3
+# print(grid_traveler(3, 2))  # 3
+# print(grid_traveler(3, 3))  # 6
+# print(grid_traveler(18, 18))  # 2333606220
+
+def optimized_grid_travel(m, n, memo={}):
+    key = str(m) + ', ' + str(n)
+
+    if key in memo:
+        return memo[key]
+
+    if m == 1 and n == 1:
+        return 1
+
+    if m == 0 or n == 0:
+        return 0
+
+    memo[key] = optimized_grid_travel(
+        m - 1, n, memo) + optimized_grid_travel(m, n - 1, memo)
+
+    return memo[key]
+
+
+print(optimized_grid_travel(18, 18))
+print(optimized_grid_travel(1, 1))
+print(optimized_grid_travel(3, 2))
