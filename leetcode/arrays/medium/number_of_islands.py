01b297dc310ee7dc865272545903c510087995bc
Caro <55109108+CaroMen@users.noreply.github.com>
2021-09-05 20:42:57 -0500

    number of islands notes started

--- leetcode/arrays/medium/number_of_islands.py
+++ leetcode/arrays/medium/number_of_islands.py
@@ -1,3 +1,17 @@
 # Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
 # An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+"""
+    INPUTS/OUTPUTS:
+        grid = [
+                    ["1","1","1","1","0"],
+                    ["1","1","0","1","0"],
+                    ["1","1","0","0","0"],
+                    ["0","0","0","0","0"]
+                ]
+
+    NOTES
+        - a single "1" surrounded by "0"s will be an island
+        -
+"""
