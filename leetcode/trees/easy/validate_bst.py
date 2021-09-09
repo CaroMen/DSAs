f09c058986dcd02601513812241ac994ade156ff
Caro <55109108+CaroMen@users.noreply.github.com>
2021-09-09 15:01:10 -0500

    validate bst notes done

--- leetcode/trees/easy/validate_bst.py
+++ leetcode/trees/easy/validate_bst.py
@@ -1,10 +1,21 @@
 """
-Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+    Given the root of a binary tree, determine if it is a valid binary search tree (BST).
 
-A valid BST is defined as follows:
+    A valid BST is defined as follows:
 
-The left subtree of a node contains only nodes with keys less than the node's key.
-The right subtree of a node contains only nodes with keys greater than the node's key.
-Both the left and right subtrees must also be binary search trees.
+    The left subtree of a node contains only nodes with keys less than the node's key.
+    The right subtree of a node contains only nodes with keys greater than the node's key.
+    Both the left and right subtrees must also be binary search trees.
 
+
+    NOTES
+        - a tree will have the root node
+            - this root node can have a left and right subtree OR it might have just one or the other
+        - if the node has a node.left, then we need to see if that value is less than our parent node's value
+            - we might need nested if statements here
+        - we do the same for the node.right
+        - since we need to do a depth first search, we need to have a stack
+        - we have to make sure that each subtree is also following the bst rules
+            - if the left node of the right subtree is less than our initial parent node, then it's not valid
+        - since we need to make sure that each subtree is valid as well we might be able to use a helper function that will check if the tree is valid
 """
