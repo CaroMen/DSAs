24976afb7c2c1a94587ee97d4fcd90c83406b93c
CaroMen <caroline_mendez@alumni.baylor.edu>
2021-10-10 20:34:14 -0500

    notes added for binary search

---
+++ leetcode/searchalgos/binarysearch.py
@@ -0,0 +1,17 @@
+"""
+    Given an array of integers nums which is sorted in ascending order and an integer target, write a function nums. If target exists, then return its index. Otherwise, return -1
+
+    You must write an algorithm with O(log n) runtime complexity
+
+
+    INPUTS/OUTPUTS
+        nums = [-1, 0, 3, 5, 9, 12], target = 9 => 4
+        nums = [-1, 0, 3, 5, 9, 12], target = 2 => -1
+
+    NOTES
+        - create a variable to find the middle of the array/list
+        - we check if the middle number equals the target number
+        - based on where the middle number falls, either less than or greater than the target number, we check the left or right side of the list
+        - repeat this process until the middle number is equal to the target number or until the list is empty
+
+"""
