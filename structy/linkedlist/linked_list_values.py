0b956fa3338b15939a91f549dbc2ce06e47c6eda
CaroMen <caroline_mendez@alumni.baylor.edu>
2021-11-09 21:14:45 -0600

    problem instructions added

---
+++ structy/linkedlist/linked_list_values.py
@@ -0,0 +1,9 @@
+"""
+  Description:
+    Write a function, linked_list_values, that takes in the head of a linked list as an argument. The function should return a list containing all values of the nodes in the linked list.
+
+  Thoughts:
+    we have to iterate through the linked list and store each value in a new list.
+    the way we can do this is to store head in a variable and we can loop while that variable is not None
+    then we can move to the next node by setting the variable to head.next
+"""
